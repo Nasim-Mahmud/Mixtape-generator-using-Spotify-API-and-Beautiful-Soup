cf34ffd048bf44cb2136aab7b2f3a6582161003a
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-09-30 05:21:12 +0600

    Web scraping data

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from bs4 import BeautifulSoup
 from datetime import date
 
 
-# year = int(input("Please enter the year: "))
-# month = int(input("Please enter the month: "))
-# day = int(input("Please enter the day: "))
-#
-# dt = date(year, month, day)   # Use datetime in case of hours, minutes and seconds
-# print(dt)
+year = int(input("Please enter the year: "))
+month = int(input("Please enter the month: "))
+day = int(input("Please enter the day: "))
 
-response = requests.get(url=f"https://www.billboard.com/charts/hot-100/{2020-11-6}")
+dt = date(year, month, day)   # Use datetime in case of hours, minutes and seconds
+print(dt)
+
+response = requests.get(url=f"https://www.billboard.com/charts/hot-100/{dt}")
 
 data = response.text
 
