23fb27f2cfb8934cc0384f1d9594b23c81c252e1
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-09-30 04:34:37 +0600

    Initializing date

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ from datetime import date
 # dt = date(year, month, day)   # Use datetime in case of hours, minutes and seconds
 # print(dt)
 
-response = requests.get(url=f"https://www.billboard.com/charts/hot-100/{2021-10-12}")
+response = requests.get(url=f"https://www.billboard.com/charts/hot-100/{2022-10-1}")
 
-print(response.text)
+data = response.text
+soup = BeautifulSoup(data, "html.parser")
+
+music = soup.find_all(name="h3", class_="c-title a-no-trucate")
+print(music)
