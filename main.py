6f70fb02db61a11b18ccb308ac5ca063105573f6
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-09-30 02:32:20 +0600

    Initializing date

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from bs4 import BeautifulSoup
 from datetime import date
 
 
-year = int(input("Please enter the year: "))
-month = int(input("Please enter the month: "))
-day = int(input("Please enter the day: "))
+# year = int(input("Please enter the year: "))
+# month = int(input("Please enter the month: "))
+# day = int(input("Please enter the day: "))
+#
+# dt = date(year, month, day)   # Use datetime in case of hours, minutes and seconds
+# print(dt)
 
-dt = date(year, month, day)   # Use datetime in case of hours, minutes and seconds
-print(dt)
+response = requests.get(url=f"https://www.billboard.com/charts/hot-100/{2021-10-12}")
 
-response = requests.get(url=f"https://www.billboard.com/charts/hot-100/{dt}")
-
+print(response.text)
