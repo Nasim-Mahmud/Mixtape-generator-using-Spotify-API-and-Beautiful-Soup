0ecc0233f54fa8eb6129d5d31b256a8f4fe2a38d
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-10-02 12:22:34 +0600

    Searching songs in Spotify

--- main.py
+++ main.py
@@ -11,18 +11,6 @@ SPOTIFY_CLIENT_ID = os.environ["SPOTIFY_CLIENT_ID"]
 SPOTIFY_CLIENT_SECRET = os.environ["SPOTIFY_CLIENT_SECRET"]
 SPOTIPY_REDIRECT_URI = "https://example.com/callback"
 
-sp = spotipy.Spotify(
-    auth_manager=SpotifyOAuth(
-        scope="playlist-modify-private",
-        redirect_uri=SPOTIPY_REDIRECT_URI,
-        client_id=SPOTIFY_CLIENT_ID,
-        client_secret=SPOTIFY_CLIENT_SECRET,
-        show_dialog=True,
-        cache_path="token.txt"
-    )
-)
-user_id = sp.current_user()["id"]
-
 # Retrieving date from user
 year = int(input("Please enter the year: "))
 month = int(input("Please enter the month: "))
@@ -50,17 +38,34 @@ for names in music_list:
     musics.append(music_name.replace("\n", ""))
 print(musics)
 
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri=SPOTIPY_REDIRECT_URI,
+        client_id=SPOTIFY_CLIENT_ID,
+        client_secret=SPOTIFY_CLIENT_SECRET,
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
+
 # Creating a list of songs URI
+
 song_uri = []
 for music in musics:
-    playlist = sp.search(q=f"track:{music} year:{year}", type="track")
+    res = sp.search(q=f"track:{music} year:{year}", type="track")
     pp = pprint.PrettyPrinter(width=41, compact=True)
-    # pp.pprint(playlist)
+    # pp.pprint(res)
     try:
-        uri = playlist["tracks"]["items"][0]["uri"]
+        uri = res["tracks"]["items"][0]["uri"]
         song_uri.append(uri)
         # pp.pprint(uri)
     except IndexError:
         print(f"Sorry! Can't find the song: {music}")
 
-print(song_uri)
+
+playlist = sp.user_playlist_create(user=user_id,name=f"{dt} Billboard Top 100", public=False)
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uri)
+print("Success!")
