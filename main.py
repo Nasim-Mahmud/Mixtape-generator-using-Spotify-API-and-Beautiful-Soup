5f86d86fbdfdb277cf2e368adce6f0f97be1214c
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-10-02 11:50:32 +0600

    Initializing Spotify credentials

--- main.py
+++ main.py
@@ -47,7 +47,6 @@ music_list = soup.find_all(name="h3", id="title-of-a-story",
 for names in music_list:
     music_name = names.get_text().replace("\t", "")
     musics.append(music_name.replace("\n", ""))
-
 print(musics)
 
 for music in musics:
